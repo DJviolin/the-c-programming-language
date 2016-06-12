d7dd0d2f3e4c1f128f64757c0a8ac64bb4df226d
DJviolin <kerozin.joe@gmail.com>
2016-06-12 23:56:21 +0200

    refact

---
+++ src/1.2-fahrenheit-floating-exercise.c
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////////////
+// Print Celsius-Fahrenheit table
+// For fahr = 0, 20, ..., 300; floating-point version
+/////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+  printf(
+      "\n/////////////////////////////////////////////////////////////\n"
+      "// This program prints Celsius to Fahrenheit conversion table\n"
+      "/////////////////////////////////////////////////////////////\n\n");
+
+  float celsius, fahr;
+  int lower, upper, step;
+
+  lower = 0;    // lower limit of temperature scale
+  upper = 300;  // upper limit
+  step = 20;    // step size
+
+  celsius = lower;
+  while (celsius <= upper) {
+    // fahr = (5.0 / 9.0) * (celsius - 32.0);
+    fahr = celsius * (9.0 / 5.0) + 32;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+
+  return 0;
+}
