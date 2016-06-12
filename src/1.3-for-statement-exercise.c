cf52dd220f706cd259e2196dc7a2443552cba400
DJviolin <kerozin.joe@gmail.com>
2016-06-13 00:16:29 +0200

    refact

---
+++ src/1.3-for-statement-exercise.c
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////
+// Print Fahrenheit-Celsius table
+// From 300 to 0
+/////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+  int fahr;
+
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+    float calc = (5.0 / 9.0) * (fahr - 32);
+    printf("%3d %6.1f\n", fahr, calc);
+  }
+}
