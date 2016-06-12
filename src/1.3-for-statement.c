542cce66abdfeabadefad2a484dc6837e852047e
DJviolin <kerozin.joe@gmail.com>
2016-06-13 00:07:19 +0200

    refact

---
+++ src/1.3-for-statement.c
@@ -0,0 +1,13 @@
+/////////////////////////////////////////////////////////////
+// Print Fahrenheit-Celsius table
+/////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+  int fahr;
+
+  for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  }
+}
