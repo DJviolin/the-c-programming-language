e43ca30bfdfe03a03cd49917c3842f85d591ab21
DJviolin <kerozin.joe@gmail.com>
2016-06-13 11:48:21 +0200

    refact

---
+++ src/1.3-for-statement-exercise-2-fn-call.c
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////
+// Print Fahrenheit-Celsius table
+// From 300 to 0
+/////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void foo(void) {
+  int fahr;
+  float calc;
+
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+    calc = (5.0 / 9.0) * (fahr - 32);
+    printf("%3d %6.1f\n", fahr, calc);
+  }
+}
+
+int main(void) {
+  foo();
+  return 0;
+}
