3681d7806d5ec199d3becbfa6b7ebd06af6e42bb
DJviolin <kerozin.joe@gmail.com>
2016-06-13 10:53:18 +0200

    refact

--- src/1.3-for-statement-exercise.c
+++ src/1.3-for-statement-exercise.c
@@ -7,9 +7,10 @@
 
 int main() {
   int fahr;
+  float calc;
 
   for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
-    float calc = (5.0 / 9.0) * (fahr - 32);
+    calc = (5.0 / 9.0) * (fahr - 32);
     printf("%3d %6.1f\n", fahr, calc);
   }
 
