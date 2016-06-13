3667d485d82677485eadd1bbb6cdcdfc627e69a1
DJviolin <kerozin.joe@gmail.com>
2016-06-14 00:18:20 +0200

    refact

--- src/1-4-symbolic-constants.c
+++ src/1-4-symbolic-constants.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+// Symbolic Constants
 #define LOWER 0    // lower limit of table
 #define UPPER 300  // upper limit
 #define STEP 20    // step size
 
 int main(void) {
-  static int fahr;
-  static float calc;
+  static int fahr;    // 0
+  static float calc;  // 0.0
 
   for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
     calc = (5.0 / 9.0) * (fahr - 32);
